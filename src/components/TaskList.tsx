f520055624e0eb5b4d76063799f427ae2d4c3e66
Luiz Henrique <7henrique18@gmail.com>
2021-05-04 11:54:05 -0300

    :white_check_mark: Tests

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -60,7 +60,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar nova tarefa"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
